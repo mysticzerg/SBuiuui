2db63beace4e7418b4debcb2c8155c226eabb5a3
mystic0 <mysticzero@gmail.com>
2020-05-27 13:56:45 +0900

    no message

---
+++ src/main/java/com/sb/uiuui/SBuiuuiApplication.java
@@ -0,0 +1,13 @@
+package com.sb.uiuui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBuiuuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBuiuuiApplication.class, args);
+	}
+
+}
